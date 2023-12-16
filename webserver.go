b3f916c55c2d340173210c025f15520e46244f56
Aidan Feess <145503050+AidanFeess@users.noreply.github.com>
2023-12-16 10:47:11 -0600

    update to compare bytes

--- webserver.go
+++ webserver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -11,7 +12,10 @@ import (
 var site_infos = make(map[string][]byte)
 
 func Startup(dir string) {
+	// Get the directory of the real HTML
 	items, _ := os.ReadDir(dir)
+
+	// Add each real HTML file to a list as byte arrays
 	for _, item := range items {
 		content, err := os.ReadFile(dir + "/" + item.Name())
 		if err != nil {
@@ -22,29 +26,36 @@ func Startup(dir string) {
 }
 
 func OnPage(link string) []byte {
-	var buf bytes.Buffer
-	// get HTML data from the website
+	// Get HTML data from the website
 	res, err := http.Get(link)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := res.Write(&buf); err != nil {
+	defer res.Body.Close()
+
+	// Read it into memory as bytes
+	res_body, err := io.ReadAll(res.Body)
+
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	return buf.Bytes()
+	return res_body
 }
 
 func main() {
 
+	// TODO: get directory base on user parameters in console when calling the file
 	dir := "C:/Users/Aidan Feess/Documents/Projects/LTSEC/scoring-engine/site_infos"
 	Startup(dir)
 
+	/// TODO: get each website somehow
 	actual := OnPage("https://info.cern.ch/")
-	bytes.Compare(site_infos["cern_info.html"], actual)
 
-	fmt.Println(site_infos["cern_info.html"])
-	fmt.Println(actual)
+	// TODO: do this for every site in site_infos
+	webserv_up := bytes.Equal(site_infos["cern_info.html"], actual)
+	fmt.Println(webserv_up)
 
 }
