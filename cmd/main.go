2259b97c3957ae2282560f1faea5ff85f4dd35b9
VJMumphrey <vjm007@email.latech.edu>
2023-12-16 01:56:05 -0600

    basic project structure plus some starter funcs

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+// Main body of the program lives here
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/LTSEC/scoring-engine/web"
+)
+
+var debug int = 1
+
+func main() {
+
+    http.HandleFunc("/", web.Root)
+
+    if debug == 1 {
+        log.Fatal(http.ListenAndServe("127.0.0.1:80", nil))
+    } else {
+        log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
+    }
+}
