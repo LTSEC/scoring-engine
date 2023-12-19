8503b023561077b03967cd08b653abe626520d3a
VJMumphrey <vjm007@email.latech.edu>
2023-12-19 12:36:56 -0600

    add echo framework

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,23 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/LTSEC/scoring-engine/web"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
-
-var debug int = 1
 
 func main() {
 
-    http.HandleFunc("/", web.Root)
+	// Echo instance
+	e := echo.New()
 
-    if debug == 1 {
-        log.Fatal(http.ListenAndServe("127.0.0.1:80", nil))
-    } else {
-        log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
-    }
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", web.Root)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
 }
