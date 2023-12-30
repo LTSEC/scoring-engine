9b35b5e10f9b639e0754584e96e7b4074fd14f92
Aidan Feess <acfeess@gmail.com>
2023-12-29 22:26:37 -0500

    edit to accept a list of ips and directory

--- webserver.go
+++ webserver.go
@@ -7,13 +7,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-var site_infos = make(map[string][]byte)
+var site_infos = make(map[int][]byte)
+var site_ips = []string{
+	"https://info.cern.ch/",
+}
 
-func Startup(dir string) {
+func Startup(dir string) { // Run on startup to read the site htmls to memory for comparison later
 	// Get the directory of the real HTML
 	items, _ := os.ReadDir(dir)
+	iter := 0
 
 	// Add each real HTML file to a list as byte arrays
 	for _, item := range items {
@@ -21,7 +26,8 @@ func Startup(dir string) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		site_infos[item.Name()] = content
+		site_infos[iter] = []byte(strings.ReplaceAll(string(content), "\n", "")) // Converting from byte to string to byte to remove stray eol
+		iter += 1
 	}
 }
 
@@ -47,15 +53,29 @@ func OnPage(link string) []byte {
 
 func main() {
 
-	// TODO: get directory base on user parameters in console when calling the file
-	dir := "C:/Users/Aidan Feess/Documents/Projects/LTSEC/scoring-engine/site_infos"
+	/// TODO: get directory base on user parameters in console when calling the file
+	/// files will need to be named according to each team
+	dir := "E:/Projects/scoring-engine/site_infos"
 	Startup(dir)
 
 	/// TODO: get each website somehow
-	actual := OnPage("https://info.cern.ch/")
+	// 	actual := OnPage("https://info.cern.ch/")
 
-	// TODO: do this for every site in site_infos
-	webserv_up := bytes.Equal(site_infos["cern_info.html"], actual)
-	fmt.Println(webserv_up)
+	/// TODO: do this for every site in site_infos
+	// webserv_up := bytes.Equal(site_infos["cern_info.html"], actual)
+	// fmt.Println(webserv_up)
+
+	for i := 0; i < len(site_ips); i++ {
+
+		pagehtml := bytes.TrimSuffix(OnPage(site_ips[i]), []byte{10})        // Trim byte 10 (eof) from end of file
+		site_info := bytes.ReplaceAll(site_infos[i], []byte{13}, []byte{10}) // Exchange byte 13 for byte 10 (im not sure why eof is at the end of every line)
+
+		fmt.Println(bytes.TrimSpace(pagehtml))
+		fmt.Println(bytes.TrimSpace(site_info))
+
+		webserv_up := bytes.Equal(bytes.TrimSpace(site_info), bytes.TrimSpace(pagehtml))
+		fmt.Println(webserv_up)
+
+	}
 
 }
