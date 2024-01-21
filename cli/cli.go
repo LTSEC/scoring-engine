d2192053ac9b7c6c90ca06385034d04ca173b3e8
NathanielMitchell <dragonfire82003@gmail.com>
2024-01-20 22:00:45 -0600

    added in bash piping for unrecognized commands & the begining of a switch for other commands

--- cli/cli.go
+++ cli/cli.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -12,17 +13,22 @@ func Cli() {
 	var userInput string
 
 	for {
-		fmt.Print("Scoring/Engine:> ")
+		var currDirectory, err = os.Getwd()
+		if err != nil {
+			fmt.Println("directory error")
+		}
+		fmt.Print(currDirectory + "$ ")
 		userInput = inputParser()
 		// slicing off the new line character for ease in manipulation and such
 		userInput = userInput[0 : len(userInput)-1]
-		fmt.Println(userInput + "\n")
+		// fmt.Println(userInput + "\n")
 		// if exit is typed, we want to exit the program
 		if userInput == "exit" {
 			break
 		}
 		userArgs := tokenizer(userInput)
-		fmt.Println(userArgs)
+
+		commandSelector(userArgs)
 	}
 }
 
@@ -43,4 +49,34 @@ func tokenizer(userInput string) []string {
 
 	return strings.Split(userInput, " ")
 
+}
+
+// switch statement for command selection
+func commandSelector(tokenizedInput []string) {
+
+	// the switch acts on the first word of the command
+	// the idea is that you'd pass the remaining args to the requisit functions
+	switch tokenizedInput[0] {
+	// test case
+	case "hello":
+		fmt.Println("it was hello!")
+	// default case to pipe into bash
+	default:
+		bashInjection(tokenizedInput)
+	}
+}
+
+// function for injecting commands into bash
+func bashInjection(command []string) {
+
+	// run command guy with exec
+	// the .. thing lets you pass a slice as if it were a hard-coded , separated list
+	cmd := exec.Command(command[0], command[1:]...)
+	// force the output of cmd to be regular stdout
+	cmd.Stdout = os.Stdout
+
+	// check for error and print
+	if err := cmd.Run(); err != nil {
+		fmt.Println("couldn't run the guy", err)
+	}
 }
