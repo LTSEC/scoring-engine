41849c5c9110f79817d4efe3eb3d44412ace34ad
VJMumphrey <vjm007@email.latech.edu>
2024-01-21 17:27:34 -0600

    move webserver to scoring folder

--- cmd/main.go
+++ cmd/main.go
@@ -12,11 +12,11 @@ var debug int = 1
 
 func main() {
 
-    http.HandleFunc("/", web.Root)
+	http.HandleFunc("/", web.Root)
 
-    if debug == 1 {
-        log.Fatal(http.ListenAndServe("127.0.0.1:80", nil))
-    } else {
-        log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
-    }
+	if debug == 1 {
+		log.Fatal(http.ListenAndServe("127.0.0.1:80", nil))
+	} else {
+		log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
+	}
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,3 @@
 module github.com/LTSEC/scoring-engine
 
 go 1.21.5
-
-require github.com/microcosm-cc/bluemonday v1.0.26
-
-require (
-	github.com/aymerick/douceur v0.2.0 // indirect
-	github.com/gorilla/css v1.0.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-)
