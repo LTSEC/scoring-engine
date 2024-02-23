85883cf6d3077f8e3f389f1bf2221e006cdbe38a
Aidan Feess <145503050+AidanFeess@users.noreply.github.com>
2024-02-23 17:19:37 -0600

    update and get functions

--- score_holder/score_holder.go
+++ score_holder/score_holder.go
@@ -1,9 +1,11 @@
 package score_holder
 
 type Map map[string]interface{}
+type ScoreMap = []map[string]map[string]interface{}
 
-var AllTeams = make([]map[string]map[string]interface{}, 0)
+var AllTeams = make(ScoreMap, 0)
 
+// Starts the score holder, takes an ORDERED LIST OF TEAMS as an input and then stores it in a map
 func Startup(teams []string) {
 	TeamMapLength := len(teams)
 	for i := 0; i < TeamMapLength; i++ {
@@ -16,10 +18,16 @@ func Startup(teams []string) {
 
 }
 
-func GetMap() {
-
+// Returns the scoremap
+func GetMap() ScoreMap {
+	return AllTeams
 }
 
-func UpdateMap() {
-
+func UpdateMap(TeamIndex int, scores Map, states Map) {
+	if scores != nil {
+		AllTeams[TeamIndex]["scores"] = scores
+	}
+	if states != nil {
+		AllTeams[TeamIndex]["states"] = states
+	}
 }
