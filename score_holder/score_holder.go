e7e3331535c5e0506093ed4cd2501bef7880c776
Aidan Feess <145503050+AidanFeess@users.noreply.github.com>
2024-02-23 17:04:24 -0600

    startup filled out

--- score_holder/score_holder.go
+++ score_holder/score_holder.go
@@ -1,6 +1,18 @@
 package score_holder
 
-func Startup() {
+type Map map[string]interface{}
+
+var AllTeams = make([]map[string]map[string]interface{}, 0)
+
+func Startup(teams []string) {
+	TeamMapLength := len(teams)
+	for i := 0; i < TeamMapLength; i++ {
+
+		NewTeam := make(map[string]map[string]interface{})
+		NewTeam["scores"] = Map{"ftp": 0, "http": 0}
+		NewTeam["states"] = Map{"ftp": false, "http": false}
+		AllTeams = append(AllTeams, NewTeam)
+	}
 
 }
 
