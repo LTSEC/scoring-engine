80df845144a5d8d8dd7d67f88cac9b8bb458b1df
Aidan Feess <145503050+AidanFeess@users.noreply.github.com>
2024-03-18 16:42:03 -0500

    added mutex and goroutines for multiteam scoring

--- scoring/scoring.go
+++ scoring/scoring.go
@@ -2,6 +2,7 @@ package scoring
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/LTSEC/scoring-engine/logging"
@@ -9,51 +10,20 @@ import (
 )
 
 var ScoringOn bool
+var mutex sync.Mutex
 
 // Starts the scoring process
-func ScoringStartup(teams []string, ftpadd int, sshadd int, httpadd int, sleeptime int) error {
+func ScoringStartup(teams []string, ftpadd int, sshadd int, httpadd int, sleeptime int, ip string, ipstartnum int) error {
 	score_holder.Startup(teams)
 	logging.CreateLogFile()
 	ScoringOn = true
 	// todo: make scoring on pause scoring instead of just stopping it entirely, so that it may be resumed later
 	for ScoringOn {
 		for index, _ := range teams {
-
-			var scoreaddftp int
-			var scoreaddssh int
-			var scoreaddhttp int
-
-			ftp, err := FTPConnect("172.29.1.5", "ftpuser", "ftppass")
-			if err != nil {
-				//logging.LogMessage("info", err.Error())
-			}
-
-			ssh, err := SSHConnect("172.29.1.5", "22", "testuser", "testpass")
-			if err != nil {
-				//logging.LogMessage("info", err.Error())
-			}
-
-			http, err := CheckWeb("C:/Users/Aidan Feess/Documents/Projects/LTSEC/scoring-engine/tests/site_infos/site_info.html", "172.29.1.5")
-			if err != nil {
-				//logging.LogMessage("info", err.Error())
-			}
-
-			if ftp != "" {
-				scoreaddftp += ftpadd
-			}
-
-			if ssh {
-				scoreaddssh += ftpadd
-			}
-
-			if http {
-				scoreaddhttp += httpadd
-			}
-
-			score_holder.UpdateTeam(index, score_holder.NewScoreMap(scoreaddftp, scoreaddhttp, scoreaddssh),
-				score_holder.NewStateMap(ftp != "", ssh, http))
+			go scoreTeam(index, ip, ipstartnum, ftpadd, sshadd, httpadd)
+			fmt.Println(score_holder.GetMap()[index])
 		}
-		fmt.Println(score_holder.GetMap()[1]["scores"])
+
 		time.Sleep(time.Duration(sleeptime) * time.Second)
 	}
 
@@ -65,4 +35,44 @@ func ScoringToggle(state bool) error {
 	//logging.LogMessage("info", "Stopping scoring")
 	ScoringOn = state
 	return nil
+}
+
+// Scores an individual team
+func scoreTeam(index int, ip string, ipstartnum int, ftpadd int, sshadd int, httpadd int) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	var scoreaddftp int
+	var scoreaddssh int
+	var scoreaddhttp int
+	conn := ip + fmt.Sprint(ipstartnum+index)
+
+	ftp, err := FTPConnect(conn, "ftpuser", "ftppass")
+	if err != nil {
+		//logging.LogMessage("info", err.Error())
+	}
+
+	ssh, err := SSHConnect(conn, "22", "testuser", "testpass")
+	if err != nil {
+		//logging.LogMessage("info", err.Error())
+	}
+
+	http, err := CheckWeb("C:/Users/Aidan Feess/Documents/Projects/LTSEC/scoring-engine/tests/site_infos/site_info.html", conn)
+	if err != nil {
+		//logging.LogMessage("info", err.Error())
+	}
+
+	if ftp != "" {
+		scoreaddftp += ftpadd
+	}
+
+	if ssh {
+		scoreaddssh += sshadd
+	}
+
+	if http {
+		scoreaddhttp += httpadd
+	}
+
+	score_holder.UpdateTeam(index, score_holder.NewScoreMap(scoreaddftp, scoreaddhttp, scoreaddssh),
+		score_holder.NewStateMap(ftp != "", ssh, http))
 }
--- scoring/scoring_test.go
+++ scoring/scoring_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestScoring(t *testing.T) {
 	go func() {
-		t.Log(ScoringStartup([]string{"Team1", "Team2"}, 5, 1, 1, 5))
+		t.Log(ScoringStartup([]string{"Team1", "Team2", "Team3", "Team4"}, 5, 1, 1, 5, "172.29.1.", 5))
 	}()
 	time.Sleep(11 * time.Second)
 	t.Log(ScoringToggle(false))
